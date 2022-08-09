32578e28d62d62f65f87ebd766f0bfeb98d6cc03
Dhruvita04 <74048191+Dhruvita04@users.noreply.github.com>
2022-08-09 20:29:30 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,14 +1,6 @@
-
-
-# In[1]:
-
-
 #Displaying "Hello World!" on the screen 
 # print is a Python defined default function, we are simply using it here print("Hello World!")
 print("Hello World!")
-
-
-# In[ ]:
 
 
 
