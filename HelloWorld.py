2bdf246a9dcee33f1ee8e1bb08d0e4f8fa9058f0
Dhruvita04 <74048191+Dhruvita04@users.noreply.github.com>
2022-08-09 20:27:34 +0530

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Displaying "Hello World!" on the screen 
+# print is a Python defined default function, we are simply using it here print("Hello World!")
+print("Hello World!")
+
+
+# In[ ]:
+
+
+
+
